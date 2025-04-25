d0c633ea6ac8cdae88a09c7edfe0971d50db7f72
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-25 18:00:10 +0600

    ✨ feat(core): add core exports
    - add core exports for routes, themes, di, services and utils

---
+++ lib/core/core_exports.dart
@@ -0,0 +1,10 @@
+export 'routes/app_routes.dart';
+export 'theme/app_themes.dart';
+export 'theme/app_colors.dart';
+export 'di/di.dart';
+export 'services/app_logger_services.dart' show LoggerServiceImpl;
+export 'utils/logger_utils.dart';
+export 'utils/navigation_utils.dart';
+
+// package exports
+export 'package:injectable/injectable.dart';
