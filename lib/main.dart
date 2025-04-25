96feef3b4142cf28d567b067114f62c10d3cc9d4
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-25 10:46:37 +0600

    ✨ feat(main): integrate dotenv for environment variables
    - load environment variables from .env file
    - use WidgetsFlutterBinding to ensure initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:e_ommerce_product_listing_app/app.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const App());
 }
