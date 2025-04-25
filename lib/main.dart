3ec6fc00f763e0f27f54677e633d080c88293700
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-25 11:02:19 +0600

    🔧 chore(dotenv): load env variables
    - load environment variables from the .env file

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:e_ommerce_product_listing_app/app.dart';
 
 Future<void> main() async {
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: '.env');
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const App());
 }
