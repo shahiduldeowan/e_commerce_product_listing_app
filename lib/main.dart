8eaa7df4fbd4644a72282d5ebbd5e028e4b20164
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-25 16:09:29 +0600

    ♻️ refactor(main): update dotenv import statement
    - change direct import to selective import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' show dotenv;
 
 import 'package:e_ommerce_product_listing_app/app.dart';
 
