214507660dc4984db8bb49c5c67fcf57ca21c559
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-25 11:12:36 +0600

    ✨ feat(product): add product pages feature
    - implement basic product page layout
    - display simple text placeholder

---
+++ lib/features/products/presentation/pages/product_pages.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProductPages extends StatelessWidget {
+  const ProductPages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Product Page')));
+  }
+}
