5a21b51fc955e3db64b5b8b65ff21d5681dbbf8d
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-25 18:00:20 +0600

    ✨ feat(app): implement initial app structure and routing
    - integrate essential packages and configurations
    - set up basic routing and theme configurations

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,7 @@
+import 'package:e_ommerce_product_listing_app/core/configs/app_configs.dart';
+import 'package:e_ommerce_product_listing_app/core/constants/route_names.dart';
+import 'package:e_ommerce_product_listing_app/core/core_exports.dart'
+    show AppRoutes, AppThemes, navigationService;
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
@@ -5,8 +9,14 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return MaterialApp(
+      title: AppConfigs.appName,
+      debugShowCheckedModeBanner: AppConfigs.debugShowCheckedModeBanner,
+      themeMode: AppConfigs.themeMode,
+      theme: AppThemes.lightTheme(),
+      navigatorKey: navigationService.navigatorKey,
+      initialRoute: RouteNames.initialPageRouteName,
+      onGenerateRoute: AppRoutes.onGenerateRoute,
     );
   }
 }
