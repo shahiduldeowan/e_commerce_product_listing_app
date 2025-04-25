f3cd4e57cab167412734c6a473a97b2ec0f2ba1d
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-25 18:02:12 +0600

    ✨ feat(navigation): add navigation utils
    - create navigation_utils.dart
    - add NavigationService to use it in app

---
+++ lib/core/utils/navigation_utils.dart
@@ -0,0 +1,4 @@
+import 'package:e_ommerce_product_listing_app/core/di/di.dart' show getIt;
+import 'package:e_ommerce_product_listing_app/core/services/navigation_services.dart';
+
+final NavigationService navigationService = getIt<NavigationService>();
