a415e7b1d06dd4311a17c195a6d1093f54b24f53
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-25 23:49:23 +0600

    ✨ feat(core): implement base use case
    - create abstract use case class
    - add call method to execute the use case with the given param

---
+++ lib/core/utils/use_case.dart
@@ -0,0 +1,12 @@
+import 'package:e_ommerce_product_listing_app/core/utils/type_defs.dart';
+
+/// An abstract class representing a use case in the application.
+///
+/// [T] is the return type of the use case.
+/// [P] is the type of the parameter passed to the use case.
+abstract class UseCase<T, P> {
+  /// Executes the use case with the given [param].
+  ///
+  /// [param] is optional and can be null for use cases that do not require parameters.
+  FutureResult<T> call({P? param});
+}
