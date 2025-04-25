b722c0a53454c22a76c7dd8b1522cf97cab122f5
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-25 18:01:59 +0600

    ✨ feat(core): add logger_utils for logging service
    - add logger_utils.dart file to provide a simple way to use logging service
    - create logger instance to use the getIt to access the LoggerService instance

---
+++ lib/core/utils/logger_utils.dart
@@ -0,0 +1,5 @@
+import 'package:e_ommerce_product_listing_app/core/di/di.dart' show getIt;
+import 'package:e_ommerce_product_listing_app/core/services/app_logger_services.dart'
+    show LoggerService;
+
+final LoggerService logger = getIt<LoggerService>();
