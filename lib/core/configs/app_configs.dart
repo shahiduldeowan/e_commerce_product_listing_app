11f79262350736adb0cfba617f34c0d94e582da4
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-25 18:00:57 +0600

    ✨ feat(config): introduce app configurations
    - add app name, debug mode, and theme mode configurations
    - centralize app settings for easier management

---
+++ lib/core/configs/app_configs.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppConfigs {
+  static const String appName = 'E-Commerce Produc Listing App';
+  static const bool debugShowCheckedModeBanner = false;
+  static const ThemeMode themeMode = ThemeMode.light;
+}
