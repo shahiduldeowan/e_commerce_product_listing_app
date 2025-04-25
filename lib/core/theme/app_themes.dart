cbc6b95d3a4ab14024e0ce19284d3372cfe638bb
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-25 16:49:56 +0600

    ✨ feat(theme): add app themes
    - add theme data to the app

--- lib/core/theme/app_themes.dart
+++ lib/core/theme/app_themes.dart
@@ -1,4 +1,5 @@
 import 'package:e_ommerce_product_listing_app/core/theme/app_colors.dart';
+import 'package:e_ommerce_product_listing_app/core/theme/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
 class AppThemes {
@@ -6,6 +7,8 @@ class AppThemes {
     return ThemeData(
       visualDensity: VisualDensity.standard,
       colorScheme: AppColors.lightColorScheme,
+      textTheme: AppTextStyles.textTheme(),
+      
     );
   }
 }
