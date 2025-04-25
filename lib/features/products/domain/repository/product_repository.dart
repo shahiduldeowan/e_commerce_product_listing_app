e733d975fdf00fad6f8c05d23cfd6c7f34cea586
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-25 23:49:47 +0600

    ✨ feat(product): create product repository
    - define abstract class ProductRepository
    - add getProducts method to fetch product list

---
+++ lib/features/products/domain/repository/product_repository.dart
@@ -0,0 +1,6 @@
+import 'package:e_ommerce_product_listing_app/core/utils/type_defs.dart';
+import 'package:e_ommerce_product_listing_app/features/products/domain/entities/product_list_entity.dart';
+
+abstract class ProductRepository {
+  FutureResult<ProductListEntity> getProducts();
+}
