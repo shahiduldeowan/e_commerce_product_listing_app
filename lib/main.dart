634d7e39032e7ae28c5fef53a39a45096bcc7df2
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-25 18:00:47 +0600

    ✨ feat(main): integrate dependency injection and logger
    - integrate get_it for dependency injection
    - configure dependencies in main.dart
    - add logger service implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' show dotenv;
-
+import 'package:e_ommerce_product_listing_app/core/core_exports.dart'
+    show configureDependencies, LoggerServiceImpl;
 import 'package:e_ommerce_product_listing_app/app.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: '.env');
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const App());
+  configureDependencies().then((_) {
+    LoggerServiceImpl.debugEnabled = true;
+    runApp(const App());
+  });
 }
