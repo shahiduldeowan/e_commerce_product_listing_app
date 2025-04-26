11a938af2a293bfbb4c4adbc996494d3db8d6b0f
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-26 17:16:23 +0600

    ✨ feat(product): add product favorite event
    - add ProductEventFavorite to handle favoriting products

--- lib/features/products/presentation/bloc/product_event.dart
+++ lib/features/products/presentation/bloc/product_event.dart
@@ -5,4 +5,5 @@ abstract class ProductEvent with _$ProductEvent {
   const factory ProductEvent.fetchInit() = ProductEventFetchInit;
   const factory ProductEvent.fetchNext() = ProductEventFetchNext;
   const factory ProductEvent.refresh() = ProductEventRefresh;
+  const factory ProductEvent.favorite(int id) = ProductEventFavorite;
 }
