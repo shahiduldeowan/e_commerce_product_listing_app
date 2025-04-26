2fafd868af084dcf3eb65197d6c1104b2639f180
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-26 17:15:17 +0600

    ✨ feat(constants): add xMidium size to AppSizes
    - add xMidium size for consistent spacing in UI elements

--- lib/core/constants/app_sizes.dart
+++ lib/core/constants/app_sizes.dart
@@ -2,6 +2,7 @@ class AppSizes {
   static const double xxSmall = 2.0;
   static const double xSmall = 4.0;
   static const double small = 8.0;
+  static const double xMidium = 10.0;
   static const double large = 16.0;
   static const double xXXLarge = 34.0;
   static const double productImageHeight = 194.0;
