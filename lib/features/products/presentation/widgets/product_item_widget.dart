9955564a89041f3fad66460c9fada8b993724357
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-26 17:16:41 +0600

    ✨ feat(products): enhance product item widget with favorite and stock status
    - add favorite button to toggle product's favorite status
    - display "out of stock" badge when product is out of stock
    - show rating and reviews count

--- lib/features/products/presentation/widgets/product_item_widget.dart
+++ lib/features/products/presentation/widgets/product_item_widget.dart
@@ -5,9 +5,12 @@ import 'package:e_ommerce_product_listing_app/core/core_exports.dart'
         AppSizes,
         AssetNames,
         ImageTypes,
-        SizeFormatExtension;
+        SizeFormatExtension,
+        outOfStock;
 import 'package:e_ommerce_product_listing_app/features/products/domain/entities/product_entity.dart';
+import 'package:e_ommerce_product_listing_app/features/products/presentation/bloc/product_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ProductItemWidget extends StatelessWidget {
   const ProductItemWidget({super.key, required this.product});
@@ -20,12 +23,58 @@ class ProductItemWidget extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       spacing: AppSizes.small,
       children: <Widget>[
-        AppImageView(
-          url: product.images?.first ?? '',
-          imageTypes: ImageTypes.network,
-          height: AppSizes.productImageHeight,
-          width: double.maxFinite,
-          radius: BorderRadius.circular(AppSizes.xSmall),
+        Stack(
+          alignment: Alignment.topRight,
+          children: <Widget>[
+            AppImageView(
+              url: product.images?.first ?? '',
+              imageTypes: ImageTypes.network,
+              height: AppSizes.productImageHeight,
+              width: double.maxFinite,
+              radius: BorderRadius.circular(AppSizes.xSmall),
+            ),
+            Positioned(
+              top: AppSizes.small,
+              right: AppSizes.small,
+              child: IconButton.filledTonal(
+                style: IconButton.styleFrom(backgroundColor: AppColors.white),
+                onPressed: () {
+                  context.read<ProductBloc>().add(
+                    ProductEventFavorite(product.id ?? -1),
+                  );
+                },
+                icon: AppImageView(
+                  url:
+                      product.isFavorite
+                          ? AssetNames.heartFilledIcon
+                          : AssetNames.heartIcon,
+                  height: AppSizes.large,
+                  width: AppSizes.large,
+                  imageTypes: ImageTypes.svg,
+                ),
+              ),
+            ),
+            if (product.isOutOfStock)
+              Positioned(
+                top: AppSizes.small,
+                left: AppSizes.small,
+                child: Container(
+                  padding: AppSizes.xSmall.toSymmetricEdgeInsets(
+                    horaizontal: AppSizes.xSmall,
+                  ),
+                  decoration: BoxDecoration(
+                    color: AppColors.red,
+                    borderRadius: AppSizes.xSmall.toRoundedBorderRadius(),
+                  ),
+                  child: Text(
+                    outOfStock,
+                    style: Theme.of(
+                      context,
+                    ).textTheme.labelLarge!.copyWith(color: AppColors.white),
+                  ),
+                ),
+              ),
+          ],
         ),
         Text(
           product.title ?? '',
@@ -67,25 +116,37 @@ class ProductItemWidget extends StatelessWidget {
           width: double.maxFinite,
           child: Row(
             spacing: AppSizes.xSmall,
-            children: [
-              Container(
-                width: AppSizes.large,
-                height: AppSizes.large,
-                decoration: BoxDecoration(
-                  color: AppColors.amber700,
-                  borderRadius: AppSizes.xxSmall.toRoundedBorderRadius(),
-                ),
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    AppImageView(
+            children: <Widget>[
+              Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  Container(
+                    width: AppSizes.large,
+                    height: AppSizes.large,
+                    decoration: BoxDecoration(
+                      color: AppColors.amber700,
+                      borderRadius: AppSizes.xxSmall.toRoundedBorderRadius(),
+                    ),
+                  ),
+                  const Positioned(
+                    child: AppImageView(
                       url: AssetNames.ratingIcon,
                       imageTypes: ImageTypes.svg,
                       height: 12,
                       width: 12,
                     ),
-                  ],
-                ),
+                  ),
+                ],
+              ),
+              Text(
+                '${product.rating}',
+                style: Theme.of(context).textTheme.labelLarge,
+              ),
+              Text(
+                '(${product.reviews?.length ?? 0.0})',
+                style: Theme.of(
+                  context,
+                ).textTheme.bodySmall!.copyWith(color: AppColors.gray600),
               ),
             ],
           ),
