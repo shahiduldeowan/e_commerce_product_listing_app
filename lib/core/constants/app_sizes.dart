79579decae860213b7a57a63d38960ffe29c2da5
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-27 00:05:31 +0600

    ✨ feat(style): add new app sizes
    - add medium size
    - add product title height

--- lib/core/constants/app_sizes.dart
+++ lib/core/constants/app_sizes.dart
@@ -3,7 +3,9 @@ class AppSizes {
   static const double xSmall = 4.0;
   static const double small = 8.0;
   static const double xMidium = 10.0;
+  static const double medium = 12.0;
   static const double large = 16.0;
   static const double xXXLarge = 34.0;
+  static const double productTitleHeight = 35.0;
   static const double productImageHeight = 194.0;
 }
