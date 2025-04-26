116486df8ce1d53c9256f09054a8479b0060b547
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-26 13:29:09 +0600

    ✨ feat(product): display products in a masonry grid
    - implement masonry grid view to display product items
    - add product item widget to show individual product details
    - adjust padding and spacing for better visual appearance

--- lib/features/products/presentation/pages/product_pages.dart
+++ lib/features/products/presentation/pages/product_pages.dart
@@ -1,6 +1,10 @@
+import 'package:e_ommerce_product_listing_app/core/core_exports.dart'
+    show AppSizes, SizeFormatExtension;
 import 'package:e_ommerce_product_listing_app/features/products/presentation/bloc/product_bloc.dart';
+import 'package:e_ommerce_product_listing_app/features/products/presentation/widgets/product_item_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class ProductPages extends StatelessWidget {
   const ProductPages({super.key});
@@ -12,15 +16,29 @@ class ProductPages extends StatelessWidget {
         return Scaffold(
           resizeToAvoidBottomInset: false,
           body: Padding(
-            padding: AppSiz,
+            padding: AppSizes.large.toHorizontalEdgeInsets(),
             child: Center(
               child: Column(
-                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Text('Product Length ${state.products.length}'),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Go to Cart'),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(border: Border.all()),
+                      width: double.maxFinite,
+                      child: MasonryGridView.count(
+                        shrinkWrap: true,
+                        crossAxisCount: 2,
+                        crossAxisSpacing: AppSizes.large,
+                        mainAxisSpacing: AppSizes.large,
+                        itemCount: state.products.length,
+                        itemBuilder: (_, int index) {
+                          return ProductItemWidget(
+                            product: state.products[index],
+                          );
+                        },
+                      ),
+                    ),
                   ),
                 ],
               ),
