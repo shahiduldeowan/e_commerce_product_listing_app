600bf67a9b89c8f20d850531e552a9870a72431a
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-26 10:01:01 +0600

    ✨ feat(routes): integrate bloc provider for product page
    - add bloc provider to inject product bloc into the product page
    - trigger initial product fetch event when the page is created

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,10 +1,18 @@
 import 'package:e_ommerce_product_listing_app/core/constants/route_names.dart';
+import 'package:e_ommerce_product_listing_app/core/di/di.dart' show getIt;
+import 'package:e_ommerce_product_listing_app/features/products/presentation/bloc/product_bloc.dart';
 import 'package:e_ommerce_product_listing_app/features/products/presentation/pages/product_pages.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppRoutes {
   static final Map<String, WidgetBuilder> _routes = <String, WidgetBuilder>{
-    RouteNames.initialPageRouteName: (_) => const ProductPages(),
+    RouteNames.initialPageRouteName:
+        (_) => BlocProvider<ProductBloc>(
+          create:
+              (_) => getIt<ProductBloc>()..add(const ProductEventFetchInit()),
+          child: const ProductPages(),
+        ),
   };
 
   static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
