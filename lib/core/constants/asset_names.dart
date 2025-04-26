f6d48c85bd80d42e9a08fb0de8e1fb3b4155a6cc
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-26 13:28:36 +0600

    ✨ feat(assets): add new icons
    - add filter, heart, rating and search icons to assets

--- lib/core/constants/asset_names.dart
+++ lib/core/constants/asset_names.dart
@@ -1,3 +1,7 @@
 class AssetNames {
   static const String noImageView = 'assets/images/no_image_view.png';
+  static const String filterIcon = 'assets/icons/filter_icon.svg';
+  static const String heartIcon = 'assets/icon/heart_icon.svg';
+  static const String ratingIcon = 'assets/icon/rating_icon.svg';
+  static const String searchIcon = 'assets/icon/search_icon.svg';
 }
