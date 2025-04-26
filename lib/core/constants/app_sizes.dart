2d0c2154ec5399785118ed64ec569f93822b32b6
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-26 13:05:40 +0600

    ✨ feat(constants): add app sizes for consistent UI
    - introduce new sizes: xSmall, small, and productImageHeight
    - rename tripleExtraLarge to xXXLarge for clarity

--- lib/core/constants/app_sizes.dart
+++ lib/core/constants/app_sizes.dart
@@ -1,4 +1,7 @@
 class AppSizes {
-  static const double large = 16;
-  static const double tripleExtraLarge = 34;
+  static const double xSmall = 4.0;
+  static const double small = 8.0;
+  static const double large = 16.0;
+  static const double xXXLarge = 34.0;
+  static const double productImageHeight = 194.0;
 }
