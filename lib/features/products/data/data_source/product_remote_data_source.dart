7651abc1b99d5a63aa0891cd63f9ef099f2d547d
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-26 10:01:43 +0600

    ✨ feat(product): implement get products by query parameter
    - add get products by query parameter functionality
    - allow client to specify limit and skip parameters

--- lib/features/products/data/data_source/product_remote_data_source.dart
+++ lib/features/products/data/data_source/product_remote_data_source.dart
@@ -13,5 +13,20 @@ abstract class ProductRemoteDataSource {
       _ProductRemoteDataSource;
 
   @GET('products')
+  /// Fetches a list of products from the remote data source.
+  ///
+  /// Returns a [Future] that completes with a [ProductListModel]
+  /// containing the list of products.
   Future<ProductListModel> getProducts();
+
+  @GET('products')
+  /// Get products by query parameters
+  ///
+  /// [limit] is the maximum number of products to return.
+  /// [skip] is the number of products to skip before returning the result.
+  ///
+  Future<ProductListModel> getProductsByQueryParam(
+    @Query('limit') int limit,
+    @Query('skip') int skip,
+  );
 }
