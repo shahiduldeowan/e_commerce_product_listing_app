bbcb0ec2c9fc491e28aede7ed6a85eebc5eb902d
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-26 17:15:26 +0600

    ✨ feat(assets): add filled heart icon
    - add new asset for filled heart icon
    - update AssetNames class to include heartFilledIcon constant

--- lib/core/constants/asset_names.dart
+++ lib/core/constants/asset_names.dart
@@ -1,7 +1,8 @@
 class AssetNames {
   static const String noImageView = 'assets/images/no_image_view.png';
   static const String filterIcon = 'assets/icons/filter_icon.svg';
-  static const String heartIcon = 'assets/icon/heart_icon.svg';
-  static const String ratingIcon = 'assets/icon/rating_icon.svg';
-  static const String searchIcon = 'assets/icon/search_icon.svg';
+  static const String heartIcon = 'assets/icons/heart_icon.svg';
+  static const String heartFilledIcon = 'assets/icons/heart_filled_icon.svg';
+  static const String ratingIcon = 'assets/icons/rating_icon.svg';
+  static const String searchIcon = 'assets/icons/search_icon.svg';
 }
