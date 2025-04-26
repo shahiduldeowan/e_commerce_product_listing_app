da2479778ed496a8931e61bc3db14b4ac0aed276
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-26 13:05:51 +0600

    ✨ feat(image): enhance image view with svg support and styling
    - add support for displaying SVG images
    - introduce color and radius properties for image customization
    - refactor image loading and error handling for better user experience

--- lib/core/widgets/app_image_view.dart
+++ lib/core/widgets/app_image_view.dart
@@ -1,9 +1,11 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:e_ommerce_product_listing_app/core/constants/asset_names.dart';
+import 'package:e_ommerce_product_listing_app/core/theme/app_colors.dart';
 import 'package:e_ommerce_product_listing_app/core/widgets/loading_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
-enum ImageTypes { network, asset }
+enum ImageTypes { network, asset, svg }
 
 class AppImageView extends StatelessWidget {
   const AppImageView({
@@ -14,6 +16,8 @@ class AppImageView extends StatelessWidget {
     this.width,
     this.height,
     this.fit = BoxFit.cover,
+    this.color,
+    this.radius,
   });
 
   final String url;
@@ -22,25 +26,54 @@ class AppImageView extends StatelessWidget {
   final double? width;
   final double? height;
   final BoxFit fit;
-  @override
-  Widget build(BuildContext context) {
-    if (imageTypes == ImageTypes.network) {
-      return CachedNetworkImage(
-        imageUrl: url,
-        width: width,
-        height: height,
-        fit: fit,
-        placeholder: (_, __) {
-          return const LoadingIndicator(type: LoadingIndicatorType.linear);
-        },
-        errorWidget: (_, __, ___) => _buildAssetImage(placeHolder),
-      );
-    }
+  final Color? color;
+  final BorderRadiusGeometry? radius;
 
-    return _buildAssetImage();
+  @override
+  Widget build(BuildContext context) => _buildCirculerImage();
+
+  Widget _buildCirculerImage() {
+    return ClipRRect(
+      borderRadius: radius ?? BorderRadius.zero,
+      child: switch (imageTypes) {
+        ImageTypes.network => _buildNetworkImage(),
+        ImageTypes.svg => _buildSvgImage(),
+        _ => _buildAssetImage(),
+      },
+    );
+  }
+
+  CachedNetworkImage _buildNetworkImage() {
+    return CachedNetworkImage(
+      imageUrl: url,
+      width: width,
+      height: height,
+      fit: fit,
+      color: color,
+      imageBuilder:
+          (_, ImageProvider<Object> imageProvider) => Container(
+            decoration: BoxDecoration(
+              color: AppColors.greyShade200,
+              image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+            ),
+          ),
+      placeholder: (_, __) {
+        return const LoadingIndicator(type: LoadingIndicatorType.linear);
+      },
+      errorWidget: (_, __, ___) => _buildAssetImage(placeHolder),
+    );
   }
 
   Image _buildAssetImage([String? assetPath]) {
-    return Image.asset(assetPath ?? url, height: height, width: width);
+    return Image.asset(
+      assetPath ?? url,
+      height: height,
+      width: width,
+      color: color,
+    );
+  }
+
+  SvgPicture _buildSvgImage() {
+    return SvgPicture.asset(url, height: height, width: width, fit: fit);
   }
 }
