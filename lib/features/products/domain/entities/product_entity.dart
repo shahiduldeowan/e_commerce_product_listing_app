e7561470ff555c1b7786e6ea7c58fe49084be096
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-26 17:16:04 +0600

    ✨ feat(product): add product entity extensions for data access
    - add extension methods to ProductEntity for easier data access and formatting
    - add methods for:
      - getting discount percentage
      - getting price
      - getting rating
      - getting review length
      - checking if product is out of stock
      - checking if product has discount
      - formatting discount percentage as string
      - formatting discount price as string

--- lib/features/products/domain/entities/product_entity.dart
+++ lib/features/products/domain/entities/product_entity.dart
@@ -30,5 +30,31 @@ abstract class ProductEntity with _$ProductEntity {
     ProductMetaEntity? meta,
     List<String>? images,
     String? thumbnail,
+    @Default(false) bool isFavorite,
   }) = _ProductEntity;
+}
+
+extension ProductEntityX on ProductEntity {
+  double get getDiscountPercentage => discountPercentage ?? 0;
+
+  double get getPrice => price ?? 0;
+
+  double get getRating => rating ?? 0;
+
+  int get getReviewLength => reviews?.length ?? 0;
+
+  bool get isOutOfStock => stock == null || stock == 0;
+
+  bool get hasDiscount => getDiscountPercentage > 0;
+
+  String get getDiscountPercentageAsString =>
+      getDiscountPercentage.toStringAsFixed(0);
+
+  String get getDiscountPriceAsString {
+    double amount =
+        hasDiscount
+            ? (getPrice * (1 - (getDiscountPercentage / 100)))
+            : getPrice;
+    return '\$${amount.toStringAsFixed(2)}';
+  }
 }
