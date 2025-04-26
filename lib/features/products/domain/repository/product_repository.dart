3283e80986cbd3f895b6be8c888b4dfb9ffdc1a8
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-26 10:02:07 +0600

    ✨ feat(product): add get products by query param
    - implement get products by query param functionality to fetch products based on pagination parameters

--- lib/features/products/domain/repository/product_repository.dart
+++ lib/features/products/domain/repository/product_repository.dart
@@ -1,6 +1,9 @@
 import 'package:e_ommerce_product_listing_app/core/utils/type_defs.dart';
+import 'package:e_ommerce_product_listing_app/features/products/domain/entities/pagination_params.dart';
 import 'package:e_ommerce_product_listing_app/features/products/domain/entities/product_list_entity.dart';
 
 abstract class ProductRepository {
-  FutureResult<ProductListEntity> getProducts();
+  FutureResult<ProductListEntity> getProductsByQueryParam(
+    PaginationParams params,
+  );
 }
