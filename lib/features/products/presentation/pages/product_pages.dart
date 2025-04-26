6a30267937155eb60bc18204f6c4c032525cd13f
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-26 10:02:52 +0600

    ✨ feat(product): display product list on product page
    - implement bloc builder to listen to product state
    - show product length in text widget
    - add a button to navigate to the cart

--- lib/features/products/presentation/pages/product_pages.dart
+++ lib/features/products/presentation/pages/product_pages.dart
@@ -1,10 +1,33 @@
+import 'package:e_ommerce_product_listing_app/features/products/presentation/bloc/product_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ProductPages extends StatelessWidget {
   const ProductPages({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Product Page')));
+    return BlocBuilder<ProductBloc, ProductState>(
+      builder: (BuildContext context, ProductState state) {
+        return Scaffold(
+          resizeToAvoidBottomInset: false,
+          body: Padding(
+            padding: AppSiz,
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text('Product Length ${state.products.length}'),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Go to Cart'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
