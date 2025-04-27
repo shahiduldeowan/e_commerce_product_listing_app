d68638ffe6671e4a1f6ab622e170e426bdac9232
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-27 07:51:59 +0600

    ✨ feat(main): implement native splash screen
    - integrates flutter_native_splash to display a splash screen during app initialization
    - configures splash screen preservation and removal to ensure seamless transition to app content

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' show dotenv;
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:e_ommerce_product_listing_app/core/core_exports.dart'
     show configureDependencies, LoggerServiceImpl;
 import 'package:e_ommerce_product_listing_app/app.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: '.env');
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   configureDependencies().then((_) {
     LoggerServiceImpl.debugEnabled = true;
     runApp(const App());
+    FlutterNativeSplash.remove();
   });
 }
